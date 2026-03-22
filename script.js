95bad3d10b9fe0b29651e12aaa01e333279d8d9b
Ryan <hensojr@gmail.com>
2026-03-22 16:04:14 -0600

    Added a fact in the football facts tool

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const footballFacts = [
   "A touchdown scores 6 points — plus you get a chance for 1 or 2 more! 🏈",
   "The NFL has 32 teams split into two conferences: NFC and AFC.",
   "The fastest NFL player ever ran 22.6 miles per hour! ⚡",
+  "The most SuperBowl wins by a single player is Tom Brady with 7"
 ];
 
 // Get elements
